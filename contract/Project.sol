d090f42afa39f28c8084b1f8e184bbc507982434
hunter <929966834@qq.com>
2020-12-17 08:02:32 +0800

    add scholar struct

--- contract/Project.sol
+++ contract/Project.sol
@@ -1,31 +1,37 @@
 pragma solidity >=0.5.0 <0.7.0;
 
 contract Project {
+    // create a new struct Scholar to store the information of scholar
+    struct Scholar {
+        uint8 id;
+        string name;
+        string description;
+        uint amount;
+    }
     
+    uint8[] public scholar_id_list;
+    // scholar list is the total list of all scholar
+    mapping (uint8 => Scholar) public scholar_list;
+    
+    // in constructor, the contract initializes the scholar list
     constructor() public {
-        // ...
+        // initialize the scholar list
+        
     }
 
     // 验证学生并发放对应的奖学金
-    function auto_distribute(string proof, address receiver) public payable {
+    function auto_distribute(uint8 index, string memory proof, address payable receiver) public payable {
         // 验证零知识
-        require(verify(proof) == true, "Proof is not correct!");
+        require(verify(index, proof) == true, "Proof is not correct!");
         
         // 查询应发奖学金的数额
-        uint256 amount = get_amount_from_proof(proof);
+        uint256 amount = scholar_list[scholar_id_list[index]].amount;
         
         // 发放奖学金给学生
         receiver.transfer(amount);
     }
     
-    function get_amount_from_proof(string proof){
-        uint256 amount;
-        // ...
-        
-        return amount;
-    }
-    
-    function verify(string proof) returns (bool) public{
+    function verify(uint8 index, string memory proof) public returns (bool) {
         // ...
         return true;
     }

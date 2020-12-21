963fc4874a4d6c5a3f3f183e07b522330452ed3f
Vel <velocfc@gmail.com>
2020-12-21 22:22:56 +0800

    Update Project.sol

--- contract/Project.sol
+++ contract/Project.sol
@@ -28,10 +28,14 @@ contract Project {
     }
 
     // 验证学生并发放对应的奖学金
-    function auto_distributeuint8 scholar_id, string memory proof1[2], string memory proof2[2][2], string memory proof3[2], string memory proof4[1], address payable receiver) public payable {
+    function auto_distribute(uint8 scholar_id, string memory proof1[2], string memory proof2[2][2], string memory proof3[2], string memory proof4[1], string receiver_string) public payable returns (bool) {
         // 验证零知识
-        require(verify(scholar_id, proof) == true, "Proof is not correct!");
+        bool success = verify(scholar_id, proof);
+        require(success == true, "Proof is not correct!");
         
+
+        address payable receiver = string2address(receiver_string);
+
         // 查询应发奖学金的数额
         uint256 amount = scholar_list[scholar_id_list[index]].amount;
         
@@ -43,10 +47,41 @@ contract Project {
         // index: 第几个奖学金
         // proof: 9个字符串形式uint256
         
+        bytes4 methodId = bytes4(keccak256("increaseAge(uint8,string)"));
         address contract_address = scholar_list[scholar_id].addr;
         
+        (bool success, bytes memory returnData) = contract_address.call(methodId, scholar_id, proof1[2], proof2[2][2], proof3[2], proof4[1]);
 
-        return true;
+        return success;
     }
 
+
+    function parseAddr(string memory _a) public pure returns (address _parsedAddress) {
+        // https://ethereum.stackexchange.com/questions/67436/a-solidity-0-5-x-function-to-convert-adress-string-to-ethereum-address
+        bytes memory tmp = bytes(_a);
+        uint160 iaddr = 0;
+        uint160 b1;
+        uint160 b2;
+        for (uint i = 2; i < 2 + 2 * 20; i += 2) {
+            iaddr *= 256;
+            b1 = uint160(uint8(tmp[i]));
+            b2 = uint160(uint8(tmp[i + 1]));
+            if ((b1 >= 97) && (b1 <= 102)) {
+                b1 -= 87;
+            } else if ((b1 >= 65) && (b1 <= 70)) {
+                b1 -= 55;
+            } else if ((b1 >= 48) && (b1 <= 57)) {
+                b1 -= 48;
+            }
+            if ((b2 >= 97) && (b2 <= 102)) {
+                b2 -= 87;
+            } else if ((b2 >= 65) && (b2 <= 70)) {
+                b2 -= 55;
+            } else if ((b2 >= 48) && (b2 <= 57)) {
+                b2 -= 48;
+            }
+            iaddr += (b1 * 16 + b2);
+        }
+        return address(iaddr);
+    }
 }
